da6b8d04303866da2e3db3d5042f30009744ac1c
codexian <codexian@yeah.net>
2018-06-08 17:06:58 +0800

    添加主题案件

---
+++ src/Theme.js
@@ -0,0 +1,24 @@
+import { Button, Layout, Modal } from "antd";
+import React, { PureComponent } from "react";
+import styles from "./Theme.css";
+
+const { Header, Footer, Content } = Layout;
+
+export default class Theme extends PureComponent {
+  handleClickTheme = () => {};
+  render() {
+    return (
+      <div>
+        <Layout>
+          <Header>Header</Header>
+          <Content className={styles.content}>
+            <Button type="primary" onClick={this.handleClickTheme}>
+              切换主题
+            </Button>
+          </Content>
+          <Footer className={styles.footer} />
+        </Layout>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import "normalize.css";
 import React from "react";
 import ReactDOM from "react-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import App from "./App";
+import Theme from "./Theme";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <HashRouter>
+    <Switch>
+      <Route component={App} exact path="/" />
+      <Route component={Theme} path="/theme" />
+    </Switch>
+  </HashRouter>,
+  document.getElementById("root")
+);
